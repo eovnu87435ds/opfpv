d82dd60060b5302c44c2d35fed529b8b29a4c429
Michael Dornisch <eovnu87435ds@mac.com>
2015-05-12 23:45:18 -0400

    Code displays oplogo on boot

--- opfpv.ino
+++ opfpv.ino
@@ -1,5 +1,19 @@
+#include <UTFT.h>
+#include <UTouch.h>
+#include <avr/pgmspace.h>
+UTFT    myGLCD(ITDB32S, 38, 39, 40, 41);
+UTouch  myTouch( 6, 5, 4, 3, 2);
+extern uint8_t SmallFont[];
+extern unsigned int opsmall[0x400];
+
 void setup() {
-  // put your setup code here, to run once:
+  myGLCD.InitLCD();
+  myGLCD.setFont(SmallFont);
+  myGLCD.clrScr();
+  myTouch.InitTouch();
+  myTouch.setPrecision(PREC_MEDIUM);
+  myGLCD.fillScr(51, 51, 51); //this is bg for OP logo
+  myGLCD.drawBitmap(105, 70, 100, 100, opsmall);
 
 }
 
